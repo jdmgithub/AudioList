d65f276a90d0431b569786417f1b0a54f435c2ef
jdmgithub <jdm_email@yahoo.com>
2014-05-08 11:47:14 -0400

    AudioList LunchMaster

---
+++ ALAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALAAppDelegate.h
+//  AudioList
+//
+//  Created by Jeffery Moulds on 5/8/14.
+//  Copyright (c) 2014 Jeffery Moulds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
