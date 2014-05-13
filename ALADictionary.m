a39904e25ecca7e3ed36cc43eecf181c6f1d3d03
jdmgithub <jdm_email@yahoo.com>
2014-05-13 09:00:19 -0400

    W7D2
    Re-added ALADictionary.m

---
+++ ALADictionary.m
@@ -0,0 +1,70 @@
+//
+//  ALADictionary.m
+//  AudioList
+//
+//  Created by Jeffery Moulds on 5/12/14.
+//  Copyright (c) 2014 Jeffery Moulds. All rights reserved.
+//
+
+#import "ALADictionary.h"
+
+@implementation ALADictionary
+
+{
+    NSMutableDictionary * dict;
+    
+}
+
+
+
+
+-(id)init
+{
+    self = [super init];
+    if (self) {
+        dict = [@{} mutableCopy];
+    }
+    return self;
+}
+
+
+
+-(id)objectForKey:(id)aKey
+{
+    return dict[aKey];
+}
+
+
+
+-(void)setObject:(id)anObject forKey:(id<NSCopying>)aKey
+{
+    dict[aKey] = anObject;
+    
+}
+
+
+
+-(void)removeObjectForKey:(id)aKey
+{
+    [dict removeObjectForKey:aKey];
+    
+}
+
+
+
+-(NSUInteger)count
+{
+    return [dict count];
+    
+}
+
+
+- (NSEnumerator *)keyEnumerator
+{
+    return [dict keyEnumerator];
+    
+}
+
+
+
+@end
